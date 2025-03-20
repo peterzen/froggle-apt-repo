a1ab974d2d853ffad32ad7ab3602b4c42e83b51c
Peter Banik <peter@froggle.org>
2025-03-20 14:38:57 +0000

    install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+REPO_URL=https://raw.githubusercontent.com/peterzen/froggle-apt-repo/master
+
+sudo apt-get install -y curl
+
+curl -s $REPO_URL/pubkey.asc | sudo apt-key add -
+
+sudo add-apt-repository -y "deb [arch=amd64] $(REPO_URL) bookworm main"
